c3677c79df0a1c808d405d20381140401b86bf91
DevPedroHB <pedroh.bergamo20@gmail.com>
2024-09-11 17:29:57 -0300

    🔥 refactor: Removido valor padrão para API_URL no envSchema

--- intermediate/web/src/env.ts
+++ intermediate/web/src/env.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 const envSchema = z.object({
-	API_URL: z.string().url().default("http://localhost:3333"),
+	API_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(import.meta.env);
